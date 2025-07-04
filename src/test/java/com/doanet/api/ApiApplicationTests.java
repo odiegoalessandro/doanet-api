ee34cf659fb1ffcb0203d4e9b644ae722ede71c2
Diego A.C. Martins <diegoalessandrodacruzmartins@gmail.com>
2025-07-04 08:18:46 -0300

    fix(docker): definindo os perfis corretos para cada parte da aplicação

--- src/test/java/com/doanet/api/ApiApplicationTests.java
+++ src/test/java/com/doanet/api/ApiApplicationTests.java
@@ -3,9 +3,11 @@ package com.doanet.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ApiApplicationTests {
 
 	@Test
